ee1f936ea91472b641b1b254c1a97de5b068df86
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2022-03-22 23:31:16 -0400

    mineral miner edits

--- src/components/MineralMiner/MineralMiner.js
+++ src/components/MineralMiner/MineralMiner.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Store } from './store';
 import { GameContainer } from './components/GameContainer/GameContainer';
+import githubIcon from '../../assets/social-links/github.svg';
 import './MineralMiner.scss';
 
-const SignUp = () => {
+const MineralMiner = () => {
 	return (
-		<div className='project-page mineral-miner-project'>
-			<Store>
-				<GameContainer />
-			</Store>
-		</div>
+		<>
+			<div className='project-page mineral-miner-project'>
+				<Store>
+					<GameContainer />
+				</Store>
+			</div>
+
+			<div className='code-container' style={{ marginLeft: 20 }}>
+				<h4>Code:</h4>
+				<a
+					className='github-project-link'
+					href='https://github.com/stefanbobrowski/Mineral-Miner'
+					target='_blank'
+					rel='noreferrer'
+					title='Global Provider - Github'>
+					<div className='github-icon'>
+						<img src={githubIcon} alt='Github'></img>
+					</div>
+					<span>Mineral Miner</span>
+				</a>
+			</div>
+		</>
 	);
 };
 
-export default SignUp;
+export default MineralMiner;
